6ff2401f7891f5d1baa71bd59d61031c958b7728
Joninho Rafinha <92312858+JoninhoRafinha@users.noreply.github.com>
2024-11-20 12:05:08 +0000

    Player Movement Done
    Added Player Movement with the new Input System

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+ 
+    private Vector2 moveInput;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y) * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movement);
+    }
+}
